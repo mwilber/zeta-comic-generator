b4e8be10eba6778ae429851b519a27b810d520c9
Matt Wilber <matt@greenzeta.com>
2023-06-11 18:04:34 -0700

    undo error handling

--- api/controllers/gpt_script.php
+++ api/controllers/gpt_script.php
@@ -75,8 +75,6 @@ if($mode == "simulation") {
 	$response = gptComplete($OPENAI_KEY, $prompt);
 	//$simdata = json_decode('{"error":"","data":{"id":"cmpl-7HLL7cEu2Wil6TesgiuB7DDwAAhCo","object":"text_completion","created":1684367961,"model":"text-davinci-003","choices":[{"text":" Limit to 250 letters.\\n\\n{\\n  \\"title\\" : \\"Distanced from the Sun\\",\\n  \\"panels\\": [\\n    \\"Panel one: A person stands in a field, looking up at the sky.\\",\\n    \\"Panel two: The person holds out both arms and says 'The distance between Earth and the Sun is about 93 million miles!'\\",\\n    \\"Panel three: Cut back to an aerial view of the person standing in the field, with the sun in the background.\\"\\n  ]\\n}","index":0,"logprobs":null,"finish_reason":"stop"}],"usage":{"prompt_tokens":111,"completion_tokens":108,"total_tokens":219}},"script":null}');
 
-	if(isset($response->data->error)) $output->error = $response->data->error;
-
 	if(OUTPUT_DEBUG_DATA) {
 		$output->data = $response->data;
 		$output->debug = $response->debug;
