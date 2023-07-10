5463ecf7b32872f47bd56c9142f193f75bc22304
Matt Wilber <matt@greenzeta.com>
2023-07-09 19:25:47 -0700

    Prompt Tweaks - Clue in the AI on the main character's species and name - Allow for possibility of characters in background.

--- api/controllers/gpt_background_3.php
+++ api/controllers/gpt_background_3.php
@@ -33,7 +33,7 @@
 		"- " . add_period($panel1),
         "- " . add_period($panel2),
         "- " . add_period($panel3),
-        "Rewrite each of the three passages as a detailed description of what the scene would look like without any characters present.",
+        "Rewrite each of the three passages as a detailed description of what the scene would look like without the main character present.",
 		"Write your response as a json object with a single property `descriptions`, which is an array of strings containing each of the descriptions.",
 		"Do not reference a panel in the description."
     );
