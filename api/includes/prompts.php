028c79e4fbdb1b65c1f707f212b50fbdf59bd6dd
Matt Wilber <matt@greenzeta.com>
2024-05-04 10:00:40 -0700

    Convert background and action prompts to heredoc

--- api/includes/prompts.php
+++ api/includes/prompts.php
@@ -31,33 +31,34 @@ The following is a description of each property value for the json object:
 `dialog`: Words spoken by Alpha Zeta. He is the only character that speaks. Do not label the dialog with a character name. This can be an empty string if the character is not speaking.
 SCRIPT;
 
+$backgroundPrompt = <<<BACKGROUND
+You are a talented artist who draws background art for animated cartoons.
+Write Dall-E prompts to draw backgrounds for three animation cells. These animation cells depict our main character, Alpha Zeta, in a scene.
+Descriptions of the three scenes are as follows:
+- {p0}
+- {p1}
+- {p2}
+ 
+Your Dall-E prompts will be written within the following rules: 
+- Describe each scene as it would look if the main character, Alpha Zeta, is not present.
+- No characters will speak to each other.
+- Do not include any items that contain readable text.
+- Do not reference a comic strip panel.
+Write the prompts as a json object with a single property `descriptions`, which is an array of strings containing each of the prompts.
+BACKGROUND;
+
+$actionPrompt = <<<ACTION
+The following statements describe a three part story.
+- {p0}
+- {p1}
+- {p2}
+For each of the three parts choose one word from the following which most closely describes the action of the main character:  {p3}.
+Write your response as a valid json object with a single property `panels`, which is an array of strings containing each of the chosen words.
+ACTION;
+
 $prompts->script = arrayFromHeredoc($scriptPrompt);
-
-$prompts->background = array(
-    "You are a talented artist who draws background art for animated cartoons. ",
-    "Write Dall-E prompts to draw backgrounds for three animation cells. These animation cells depict our main character, Alpha Zeta, in a scene.",
-    "Descriptions of the three scenes are as follows:",
-    "- {p0}",
-    "- {p1}",
-    "- {p2}",
-    " ",
-    "Your Dall-E prompts will be written within the following rules: ",
-    "- Describe each scene as it would look if the main character, Alpha Zeta, is not present.",
-    "- No characters will speak to each other.",
-    "- Do not include any items that contain readable text.",
-    "- Do not reference a comic strip panel.",
-    "Write the prompts as a json object with a single property `descriptions`, which is an array of strings containing each of the prompts."
-);
-
-$prompts->action = array(
-    "The following statements describe a three part story.",
-    "- {p0}",
-    "- {p1}",
-    "- {p2}",
-    "For each of the three parts coose one word from the following which most closely describes the action of the main character: ",
-    "{p3}.",
-    "Write your response as a valid json object with a single property `panels`, which is an array of strings containing each of the chosen words."
-);
+$prompts->background = arrayFromHeredoc($backgroundPrompt);
+$prompts->action = arrayFromHeredoc($actionPrompt);
 
 function generatePrompt($instructions, $values) {
 
