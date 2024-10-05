4592501eab19fdec10be3c755f5bb08bb02dccfe
Matt Wilber <matt@greenzeta.com>
2024-10-04 17:17:43 -0700

    Update gpt to gpt-4o-2024-08-06

--- api/models/gpt.php
+++ api/models/gpt.php
@@ -6,8 +6,9 @@ class ModelGpt {
 	function __construct() {
 		// $this->modelName = "gpt-3.5-turbo-16k";
 		// $this->modelName = "gpt-4";
-		//$this->modelName = "gpt-4-1106-preview";
-		$this->modelName = "gpt-4o-2024-05-13";
+		// $this->modelName = "gpt-4-1106-preview";
+		// $this->modelName = "gpt-4o-2024-05-13";
+		$this->modelName = "gpt-4o-2024-08-06";
 		$this->apiUrl = "https://api.openai.com/v1/chat/completions";
 		$this->apiKey = OPENAI_KEY;
 	}
