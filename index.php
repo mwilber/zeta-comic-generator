1adecb5adbeb6120ca2cdf689f717f94eb9e0be0
Matt Wilber <matt@greenzeta.com>
2025-03-07 06:30:53 -0800

    Bump front-end version to 3.0.0

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 	$request = $_SERVER['REQUEST_URI'];
 	//echo $request;
 
-	$version = "2.0.2";
+	$version = "3.0.0";
 	$meta = new stdClass();
 	$meta->siteTitle = "Zeta Comic Generator";
 	$meta->title = "Zeta Comic Generator";
