db060178c1cf1efadd0372036742498968a3782b
Matt Wilber <matt@greenzeta.com>
2025-03-21 17:05:37 -0700

    cleanup dalle model

--- api/models/dall.php
+++ api/models/dall.php
@@ -11,22 +11,6 @@ class ModelDallE extends BaseModel {
 		$this->imageSize = "1024x1024";
 	}
 
-	// function sendPrompt($prompt) {
-		
-	// 	$result = new stdClass;
-	// 	$response = $this->textToImage($prompt);
-	// 	$json = json_decode($response);
-	// 	$result->data = $json;
-
-	// 	$result->error = $json->error;
-
-	// 	if (isset($json->data[0])) {
-	// 		$result->json = $json->data[0];
-	// 	}
-
-	// 	return $result;
-	// }
-
 	protected function buildRequestBody($prompt) {
 		$messagesArray = [];
 		foreach ($messages as $message) {
@@ -48,35 +32,6 @@ class ModelDallE extends BaseModel {
 
 		return $body;
 	}
-
-	// function textToImage($prompt) {
-
-	// 	$ch = curl_init();
-	// 	$headers = array(
-	// 		'Authorization: Bearer ' . $this->apiKey,
-	// 		'Content-Type: application/json',
-	// 	);
-	// 	curl_setopt($ch, CURLOPT_URL, $this->apiUrl);
-	// 	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-	// 	curl_setopt($ch, CURLOPT_HEADER, 0);
-	// 	$body = '{
-	// 		"model": "'.$this->modelName.'",
-	// 		"prompt": "'.$prompt.'",
-	// 		"n": 1,
-	// 		"size": "'.$this->imageSize.'"
-	// 	}';
-
-	// 	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST"); 
-	// 	curl_setopt($ch, CURLOPT_POSTFIELDS,$body);
-	// 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-	// 	// Timeout in seconds
-	// 	curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-	
-	// 	$response = curl_exec($ch);
-
-	// 	return $response;
-	// }
 
 	protected function processResponse($response) {
 		$result = new stdClass;
