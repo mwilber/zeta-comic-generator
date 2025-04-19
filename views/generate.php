772de2c8b3573eda206434360cdf35c6e85b3e9b
Matt Wilber <matt@greenzeta.com>
2025-04-18 17:38:08 -0700

    Upgrade to o3 and GPT 4.1

--- views/generate.php
+++ views/generate.php
@@ -35,8 +35,7 @@
 					<div class="select">
 						<select name="story-model" id="story-model">
 							<option value="">(Select a model)</option>
-							<option value="gpt">GPT 4o</option>
-							<option value="o">o3 Mini</option>
+							<option value="o">o3</option>
 							<option value="grok">Grok 2</option>
 							<option value="claude">Claude Sonnet 3.5</option>
 							<option value="deepseekr">DeepSeek R1</option>
@@ -51,8 +50,9 @@
 					<div class="select">
 						<select name="script-model" id="script-model">
 							<option value="">(Select a model)</option>
-							<option value="gpt">GPT 4o</option>
+							<option value="gpt">GPT 4.1</option>
 							<option value="grok">Grok 2</option>
+							<option value="claude">Claude Sonnet 3.5</option>
 							<option value="gem">Gemini 1.5</option>
 							<option value="deepseek">DeepSeek V3</option>
 							<option value="llama">Llama 3</option>
