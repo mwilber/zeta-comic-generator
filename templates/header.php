bd1d4d4ec9eb8ada94f9c982e59fa3a84c984f95
Matt Wilber <matt@greenzeta.com>
2025-08-01 11:46:21 -0700

    Remove duplicate about from main nav

--- templates/header.php
+++ templates/header.php
@@ -25,10 +25,6 @@
 			<img class="burst" src="/assets/images/burst.svg" alt="Cartoon burst icon" />
 			<span class="cartoon-font">About</span>
 		</a>
-		<a href="/about" class="cartoon-button">
-			<img class="burst" src="/assets/images/burst.svg" alt="Cartoon burst icon" />
-			<span class="cartoon-font">About</span>
-		</a>
 	</nav>
 	<a href="/"><h1>Zeta Comic Generator</h1></a>
 </header>
