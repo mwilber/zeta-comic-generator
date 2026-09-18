f483a649fd4f1da3f1e99163fab429a0fef97d3d
Matthew Wilber <mwilber@gmail.com>
2026-09-17 21:01:45 -0400

    Style comic items and wrap series thumbnails

--- views/series.php
+++ views/series.php
@@ -45,7 +45,9 @@ if ($series):
 				<?php foreach($comics as $index => $comic): ?>
 					<li class="comic-item">
 						<a href="/detail/<?= htmlspecialchars($comic['permalink']) ?>">
-							<img src="<?php echo BUCKET_URL; ?>/thumbnails/thumb_<?= htmlspecialchars($comic['permalink']) ?>.png" alt="<?= htmlspecialchars($comic['title']) ?>" width="100" />
+							<span class="series-thumbnail">
+								<img src="<?php echo BUCKET_URL; ?>/thumbnails/thumb_<?= htmlspecialchars($comic['permalink']) ?>.png" alt="<?= htmlspecialchars($comic['title']) ?>" width="100" height="100" />
+							</span>
 							<span class="comic-title">
 								Part <?= $totalComics - $index ?>
 								<br/>
